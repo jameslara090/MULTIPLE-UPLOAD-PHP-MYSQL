9a2fe1172d3bc22a4bb2f8c83582ac1464e0d452
jameslara090 <james.lee.lara@gmail.com>
2022-08-31 11:02:53 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,5 @@
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select Image Files to Upload:
+    <input type="file" name="files[]" multiple >
+    <input type="submit" name="submit" value="UPLOAD">
+</form>
